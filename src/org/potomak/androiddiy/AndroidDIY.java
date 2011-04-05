948af78c430a50913001ca2e6bd51a8edc47e840
Giovanni <potomak84@gmail.com>
2011-04-05 13:26:38 +0200

    project import

---
+++ src/org/potomak/androiddiy/AndroidDIY.java
@@ -0,0 +1,13 @@
+package org.potomak.androiddiy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidDIY extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
